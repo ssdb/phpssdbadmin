8d65640ef274453e7954c3b39b4f3fe72b84b8b3
ideawu <ideawu@local>
2014-08-10 09:44:59 +0800

    update

--- app/views/layout.tpl.php
+++ app/views/layout.tpl.php
@@ -39,6 +39,7 @@
 	<?php _view(); ?>
 		
 	<div class="footer">
+		<!-- you are licensed to edit this message -->
 		Copyright&copy;2014 <a href="http://www.ideawu.net/">ideawu</a>. All rights reserved.
 		<?php printf('%.2f', 1000*(microtime(1) - APP_TIME_START)); ?> ms
 	</div>
